a2f003e213e93a0a7ca184fd08df3d12cfae0740
Agnieszka Szczepanik <agnieszkaszczepanik93@gmail.com>
2024-09-18 12:14:46 +0200

    task 5

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,15 @@
-
+const login = document.querySelector(".login-form");
+login.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const email = login.elements.email.value.trim();
+  const password = login.elements.password.value.trim();
+  if (email === "" || password === "") {
+    return;
+  }
+  const formData = {
+    email: email,
+    password: password,
+  };
+  console.log(formData);
+  login.reset();
+});
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const changeColor = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+changeColor.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  color.textContent = newColor;
+});
